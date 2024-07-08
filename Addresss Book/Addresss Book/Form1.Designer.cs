330d86c43929421c020eb7fae155718561d3ea7e
Paps004 <spsetutsi@st.ug.edu.gh>
2024-07-07 14:48:23 -1200

    Address Book

---
+++ Addresss Book/Addresss Book/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Addresss_Book
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(430, 146);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(253, 38);
+            this.txtname.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(430, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SAVE";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtphone
+            // 
+            this.txtphone.Location = new System.Drawing.Point(430, 307);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(253, 38);
+            this.txtphone.TabIndex = 2;
+            this.txtphone.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtmail
+            // 
+            this.txtmail.Location = new System.Drawing.Point(430, 225);
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(253, 38);
+            this.txtmail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(158, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Full Name ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(158, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 32);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "E-mail address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(158, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label4.Location = new System.Drawing.Point(313, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ADDRESS BOOK";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-175, -154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1169, 944);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(745, 529);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
